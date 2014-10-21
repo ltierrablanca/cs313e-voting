23b0c24d10b0b8a3bf293c6afb16aeebee98004a
Luis Tierrablanca <ltierrablanca@yahoo.com>
2014-10-21 17:57:10 -0500

    Voting.py can now correctly identify a winner with over half of the votes

--- Voting.py
+++ Voting.py
@@ -1,54 +1,39 @@
+# will simulate a candidate and keep track of the candidate's name, number of votes 
+# it has and which ballots are currently choosing this candidate
 class Candidate:
 
-	def __init__ (self, name, num):
+    def __init__ (self, name, num):
 	    self.name = name
-	    self.num= num
+	    self.num = num
 	    self.ballots = []
 
-	def getvotes(self):
-		return len(self.ballots)
+    # will return the current number of votes for the candidate
+    def getvotes(self):
+	    return len(self.ballots)
 
-	def __str__ (self):
-		return "Candidate " + str(self.num) + ", " + self.name + ", has " + str(self.getvotes()) + " votes."
+    def getname(self):
+        return self.name
 
+    def __str__ (self):
+        return "Candidate " + str(self.num) + ", " + self.name + ", has " + str(self.getvotes()) + " votes."
 
+# this class will simulate a ballot and keeps track of the current candidate choice
 class Ballot:
-
+    # constructor for ballot class
     def __init__(self, votes):
 	    self.votes = votes.split()
 	    self.choice = 0
 
+    # will return the current choice on the ballot
     def getchoice(self):
     	return self.votes[self.choice]
-
+    
+    # will adjust the current choice on the ballot
     def adjustchoice(self):
     	self.choice += 1
 
-    def __str__(self):
-    	return str(self.votes)
-
-# candidates = []
-
-# for i in range(num_candidates):
-# 	s = r.readline()
-# 	s = s.strip()
-# 	x = Candidate(s, i)
-# 	candidates.append(x)
-	
-# for i in range(num_ballots):
-# 	s = r.readline()
-# 	s = s.strip()
-# 	x = Ballot(s)
-# 	choice = x.getchoice()
-# 	candidates[choice - 1].ballots.append(x)
-# 	numvotes += 1
-
-
-
-
-
-#this method will do all of reading
-
+    
+# will read the input and build the ballot and candidate objects
 def voting_read(r, w):
     line = r.readline()
     line = line.strip()
@@ -76,9 +61,33 @@ def voting_read(r, w):
             line = r.readline()
             line = line.strip() 
         
-        for can in candidates:
-        	print(str(can))
+        winner = det_winner(candidates, num_votes, num_candidates)
+        for can in winner:
+            print(can)
+        if((i + 1) != cases):
+            print()
 
+
+# will determine the winner of the election
+def det_winner(candidates, num_votes, num_candidates):
+        all_votes = []
+        winner = []
+        for can in candidates:
+        	all_votes.append(can.getvotes())
+        top = max(all_votes)
+        if(top > (num_votes / 2)):
+            index = all_votes.index(top)
+            winner.append(candidates[index].getname())
+            return winner
+        # elif(top == (num_votes / num_candidates)):
+        #     for can in candidates:
+        #         winner.append(can.getname())
+        #         return 
+        # else:
+        #     losers = []
+        #     for num in all_votes:
+        #         if (top >= num):
+        #             losers.append(num)
 
 
 
