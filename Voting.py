47bf98f001581248faec42e86602b3c6747a6d1f
Luis Tierrablanca <ltierrablanca@yahoo.com>
2014-10-21 19:27:41 -0500

    redis_votes is currently broken and produces an IndexError

--- Voting.py
+++ Voting.py
@@ -1,21 +1,36 @@
 # will simulate a candidate and keep track of the candidate's name, number of votes 
 # it has and which ballots are currently choosing this candidate
 class Candidate:
-
+    
+    # constructor for the candidate class
     def __init__ (self, name, num):
 	    self.name = name
 	    self.num = num
 	    self.ballots = []
 
+    #will add a ballot object to a candidates ballots
+    def addballot(self, ballot):
+        self.ballots.append(ballot)
+
     # will return the current number of votes for the candidate
-    def getvotes(self):
+    def getnumvotes(self):
 	    return len(self.ballots)
 
+    # will return the ballots for this candidate
+    def getballots(self):
+        return self.ballots
+
+    # will return the candidates name
     def getname(self):
         return self.name
 
+    # will return the candidates number
+    def getnum(self):
+        return self.num
+
+    # will return string version of the candidate
     def __str__ (self):
-        return "Candidate " + str(self.num) + ", " + self.name + ", has " + str(self.getvotes()) + " votes."
+        return "Candidate " + str(self.num) + ", " + self.name + ", has " + str(self.getnumvotes()) + " votes."
 
 # this class will simulate a ballot and keeps track of the current candidate choice
 class Ballot:
@@ -39,28 +54,25 @@ def voting_read(r, w):
     line = line.strip()
     cases = int(line)
     line = r.readline()
-
-    for i in range(cases):
+    for i in range(cases):              # run through all of the cases
         candidates = []
         line = r.readline()
         line = line.strip()
         num_candidates = int(line)
-        for j in range(num_candidates):
+        for j in range(num_candidates):             # create candidate objects
             line = r.readline()
             line = line.strip()
             candidates.append(Candidate(line, j + 1))
-
         line = r.readline()
         line = line.strip()
         num_votes = 0
-        while(line != ""):
+        while(line != ""):              # create ballot objects
             b = Ballot(line)
             choice = int(b.getchoice())
-            candidates[choice - 1].ballots.append(b)
+            candidates[choice - 1].addballot(b)
             num_votes += 1
             line = r.readline()
             line = line.strip() 
-        
         winner = det_winner(candidates, num_votes, num_candidates)
         for can in winner:
             print(can)
@@ -68,26 +80,46 @@ def voting_read(r, w):
             print()
 
 
-# will determine the winner of the election
+# will determine the winner of the election and return it as a list
 def det_winner(candidates, num_votes, num_candidates):
-        all_votes = []
-        winner = []
+    can_votes = []
+    winner = []
+    for can in candidates:
+    	can_votes.append(can.getnumvotes())
+    top = max(can_votes)
+    if(top > (num_votes / 2)):              #candidate has over 50% of the votes
+        index = can_votes.index(top)
+        winner.append(candidates[index].getname())
+        return winner
+    elif(top == (num_votes / num_candidates)):              #all candidates are tied
         for can in candidates:
-        	all_votes.append(can.getvotes())
-        top = max(all_votes)
-        if(top > (num_votes / 2)):
-            index = all_votes.index(top)
-            winner.append(candidates[index].getname())
-            return winner
-        elif(top == (num_votes / num_candidates)):
+            winner.append(can.getname())
+        return winner
+    else:
+        redis_votes(candidates, num_votes)
+
+
+# will redistribute the ballots that chose a the candidate with the least votes
+def redis_votes(candidates, num_votes):
+    can_votes = []
+    for can in candidates:              #create list with all of the candidates' vote totals
+        can_votes.append(can.getnumvotes())
+    low = min(can_votes)
+    index = can_votes.index(low)
+    loser = candidates.pop(index)
+    ballots = loser.getballots()
+    for x in ballots:
+        x.adjustchoice()
+    while(len(ballots) != 0):
+        for x in ballots:
+            valid = False
+            choice = x.getchoice()
             for can in candidates:
-                winner.append(can.getname())
-            return winner
-        # else:
-        #     losers = []
-        #     for num in all_votes:
-        #         if (top >= num):
-        #             losers.append(num)
-
-
-
+                if(choice == can.getnum()):
+                    valid = True
+                    can.addballot(x)
+                    ballots.remove(x)
+                    break
+            if(not(valid)):
+                x.adjustchoice()
+    det_winner(candidates, num_votes, len(candidates))
