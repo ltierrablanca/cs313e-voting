3aa1d3e196e14f0e27f85c92aef74dc315c690c0
Luis Tierrablanca <ltierrablanca@yahoo.com>
2014-10-21 19:58:38 -0500

    Ballot class now has 2 tests

--- TestVoting.py
+++ TestVoting.py
@@ -12,16 +12,41 @@
 
 from unittest import main, TestCase
 
-from Voting import 
+from Voting import Candidate, Ballot, det_winner, redis_votes
 
 # -----------
-# TestCollatz
+# TestVoting
 # -----------
 
 class TestVoting (TestCase) :
     
+    # -------------
+    # Ballot
+    # -------------
 
-    def test_voting (self) :
+    def test_Ballot1 (self):
+        b = Ballot("1 2 3")
+        self.assertEqual(str(b), "1 2 3")
+
+    def test_Ballot2 (self):
+        b = Ballot("1 2 3")
+        self.assertEqual(b.getchoice(), '1')
+    
+    # -------------
+    # Candidate
+    # -------------
+
+    # def test_Candidate1 (self):
+    #     c = Candidate("John Doe", 1)
+    #     b = Ballot("1 2 3")
+    #     can.addballot(b)
+
+    
+
+    # def test_det_winner1 (self) :
+
+
+    # def test_redis_votes1 (self):
         
 
 # ----
