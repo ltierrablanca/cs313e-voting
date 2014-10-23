8dfdd987a5f42e9b611565e12a5b20e7ab2141ec
Luis Tierrablanca <ltierrablanca@yahoo.com>
2014-10-22 21:51:31 -0500

    Voting.py correctly identifies winner. TestVoting now contains many unit tests.

--- TestVoting.py
+++ TestVoting.py
@@ -10,9 +10,10 @@
 # imports
 # -------
 
+from io       import StringIO
 from unittest import main, TestCase
 
-from Voting import Candidate, Ballot, det_winner, redis_votes
+from Voting import Candidate, Ballot, voting_read, det_winner, redis_votes
 
 # -----------
 # TestVoting
@@ -30,23 +31,138 @@ class TestVoting (TestCase) :
 
     def test_Ballot2 (self):
         b = Ballot("1 2 3")
+        self.assertEqual(type(b.getchoice()), str)
+
+    def test_Ballot3 (self):
+        b = Ballot("1 2")
         self.assertEqual(b.getchoice(), '1')
+
+    def test_Ballot4 (self):
+        b = Ballot("1 2")
+        b.adjustchoice()
+        self.assertEqual(b.getchoice(), '2')
     
     # -------------
     # Candidate
     # -------------
 
-    # def test_Candidate1 (self):
-    #     c = Candidate("John Doe", 1)
-    #     b = Ballot("1 2 3")
-    #     can.addballot(b)
+    def test_Candidate1 (self):
+        c = Candidate("John Doe", 1)
+        b = Ballot("1 2 3")
+        c.addballot(b)
+        self.assertEqual(c.ballots[0].votes, ['1', '2', '3'])
 
-    
+    def test_Candidate2 (self):
+        c = Candidate("John Doe", 1)
+        b = Ballot("1 2 3")
+        c.addballot(b)
+        b = Ballot("1 3 2")
+        c.addballot(b)
+        self.assertEqual(c.ballots[1].votes, ['1', '3', '2'])
 
-    # def test_det_winner1 (self) :
+    def test_Candidate3 (self):
+        c = Candidate("John Doe", 1)
+        self.assertEqual(c.getnumvotes(), 0)
+
+    def test_Candidate4 (self):
+        c = Candidate("John Doe", 1)
+        c.addballot(Ballot("1 2 3"))
+        self.assertEqual(c.getnumvotes(), 1)
+
+    def test_Candidate5 (self):
+        c = Candidate("John Doe", 1)
+        s = "Candidate 1, John Doe, has 0 votes."
+        self.assertEqual(str(c), s)
+
+    # --------------
+    # voting_read
+    # --------------
+
+    def test_read1 (self):
+        r = StringIO("1\n\n2\nJohn Johnson\nJane Smith\n1 2\n2 1\n1 2\n")
+        w = StringIO()
+        voting_read(r, w)
+        self.assertEqual(w.getvalue(), 'John Johnson\n')
+
+    def test_read2 (self):
+        r = StringIO("1\n\n3\nJohn Johnson\nJane Smith\nBob Doe\n1 2 3\n2 1 3\n1 2 3\n2 1 3\n 3 1 2")
+        w = StringIO()
+        voting_read(r, w)
+        self.assertEqual(w.getvalue(), 'John Johnson\n')
 
 
-    # def test_redis_votes1 (self):
+    # --------------
+    # det_winner
+    # --------------
+
+    def test_det_winner1 (self) :
+        can = []
+        c = Candidate("John Doe", 1)
+        c.addballot(Ballot("1 2"))
+        can.append(c)
+        c = Candidate("Jane Johnson", 2)
+        c.addballot(Ballot("2 1"))
+        can.append(c)
+        winner = det_winner(can, 2)
+        self.assertEqual(winner, ['John Doe', 'Jane Johnson'])
+
+
+    def test_det_winner2 (self) :
+        can = []
+        c = Candidate("John Doe", 1)
+        c.addballot(Ballot("1 2"))
+        c.addballot(Ballot("1 2"))
+        can.append(c)
+        c = Candidate("Jane Johnson", 2)
+        c.addballot(Ballot("2 1"))
+        can.append(c)
+        winner = det_winner(can, 3)
+        self.assertEqual(winner, ['John Doe'])
+
+    # -------------
+    # redis_votes
+    # -------------
+
+    def test_redis_votes1 (self):
+        can = []
+        c = Candidate("John Doe", 1)
+        c.addballot(Ballot("1 2 3 4"))
+        c.addballot(Ballot("1 2 3 4"))
+        can.append(c)
+        c = Candidate("Jane Smith", 2)
+        c.addballot(Ballot("2 1 3 4"))
+        c.addballot(Ballot("2 1 3 4"))
+        can.append(c)
+        c = Candidate("Srihan Srihan", 3)
+        c.addballot(Ballot("4 3 2 1"))
+        can.append(c)
+        c = Candidate("Jack Johnson", 4)
+        c.addballot(Ballot("3 4 1 2"))
+        can.append(c)
+        can = redis_votes(can, 6)
+        winners = []
+        for c in can:
+            winners.append(c.name)
+        self.assertEqual(winners, ['John Doe', 'Jane Smith'])
+
+    def test_redis_votes2 (self):
+        can = []
+        c = Candidate("John Doe", 1)
+        c.addballot(Ballot("1 2 3"))
+        c.addballot(Ballot("1 2 3"))
+        can.append(c)
+        c = Candidate("Jane Smith", 2)
+        c.addballot(Ballot("2 1 3"))
+        c.addballot(Ballot("2 1 3"))
+        can.append(c)
+        c = Candidate("Srihan Srihan", 3)
+        c.addballot(Ballot("3 2 1"))
+        can.append(c)
+        can = redis_votes(can, 5)
+        winners = []
+        for c in can:
+            winners.append(c.name)
+        self.assertEqual(winners, ['John Doe', 'Jane Smith'])
         
 
 # ----
--- Voting.py
+++ Voting.py
@@ -42,7 +42,7 @@ class Ballot:
         s = s + self.votes[len(self.votes) - 1]
         return s
 
-    
+
 # will read the input and build the ballot and candidate objects
 def voting_read(r, w):
     line = r.readline()
@@ -68,53 +68,71 @@ def voting_read(r, w):
             num_votes += 1
             line = r.readline()
             line = line.strip() 
-        winner = det_winner(candidates, num_votes, num_candidates)
-        for can in winner:
-            print(can)
+        winners = det_winner(candidates, num_votes)
+        for can in winners:
+            w.write(can)
+            w.write('\n')
         if((i + 1) != cases):
-            print()
+            w.write('\n')
 
 
 # will determine the winner of the election and return it as a list
-def det_winner(candidates, num_votes, num_candidates):
-    can_votes = []
-    winner = []
-    for can in candidates:
-    	can_votes.append(can.getnumvotes())
-    top = max(can_votes)
-    if(top > (num_votes / 2)):              #candidate has over 50% of the votes
-        index = can_votes.index(top)
-        winner.append(candidates[index].name)
-        return winner
-    elif(top == (num_votes / num_candidates)):              #all candidates are tied
-        for can in candidates:
-            winner.append(can.name)
-        return winner
-    else:
-        redis_votes(candidates, num_votes)
+def det_winner(candidates, num_votes):
+    assert len(candidates) != 0             #there must be at least one candidate
+    assert num_votes != 0               #there must be at least one vote
+    done = False
+    while(not(done)):               #while not done
+        can_votes = []
+        winner = []
+        num_candidates = len(candidates)
+        for can in candidates:              #create can_votes, which contains the vote totals
+        	can_votes.append(can.getnumvotes())
+        top = max(can_votes)                #top has the highest vote count
+        if(top > (num_votes / 2)):              #a candidate has over 50% of the votes
+            index = can_votes.index(top)
+            winner.append(candidates[index].name)               #winner now contains candidate with most votes
+            assert len(winner) != 0             #winner must contain at least one name
+            return winner
+        elif(top == (num_votes / num_candidates)):              #all candidates are tied
+            for can in candidates:
+                winner.append(can.name)             #winner now contains all candidate names
+            assert len(winner) != 0             #winner must contain at least one name
+            return winner
+        else:
+            candidates = redis_votes(candidates, num_votes)             #redistribute the loser's votes
 
 
-# will redistribute the ballots that chose a the candidate with the least votes
+# will redistribute the ballots that chose the candidate with the least votes
 def redis_votes(candidates, num_votes):
+    assert len(candidates) != 0
+    assert num_votes != 0
     can_votes = []
     for can in candidates:              #create list with all of the candidates' vote totals
         can_votes.append(can.getnumvotes())
-    low = min(can_votes)
-    index = can_votes.index(low)
-    loser = candidates.pop(index)
-    ballots = loser.ballots
-    for x in ballots:
+    low = min(can_votes)                #low has the lowest vote count
+    losers = []
+    for can in candidates:              #remove losers from candidates and create a list with them
+        if(can.getnumvotes() == low):
+            losers.append(can)
+    for can in losers:
+        candidates.remove(can)
+    ballots = []
+    for can in losers:              #get all ballots that voted for the losers
+        for b in can.ballots:
+            ballots.append(b)
+    for x in ballots:               #adjust the choice of all of the ballots that voted for losers
         x.adjustchoice()
-    while(len(ballots) != 0):
+    while(len(ballots) > 0):        #while there are ballots left to adjust
         for x in ballots:
             valid = False
             choice = x.getchoice()
             for can in candidates:
-                if(choice == can.num):
+                if(choice == str(can.num)):             #if the adjusted ballot choice is a candidate
                     valid = True
-                    can.addballot(x)
-                    ballots.remove(x)
+                    can.addballot(x)                #give ballot to new candidate
+                    ballots.remove(x)               #remove from ballots left to adjust
                     break
-            if(not(valid)):
+            if(not(valid)):             #havent found a valid candidate yet, adjust choice again
                 x.adjustchoice()
-    det_winner(candidates, num_votes, len(candidates))
+    assert len(candidates) != 0
+    return candidates
